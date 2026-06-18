9c8ae47eee3d4e6219c9e7e3cabbe42dd8e0f271
Nico Werner <nico03werner@gmail.com>
2026-06-18 10:34:37 +0200

    refactor: extract webhook image references

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type ImageReference = {
+  url: string;
+  markdown: string;
+  source: string;
+  context: string;
+  visionSummary?: string;
+};
+
+export type ImageSource = {
+  text: string;
+  source: string;
+};
